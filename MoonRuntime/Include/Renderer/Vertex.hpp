398acb32deeefe4cb16c6ddb80f5be9036b67e96
Jararered <Jararered@icloud.com>
2023-08-02 14:28:53 -0400

    small bug fix and type defs

--- MoonRuntime/Include/Renderer/Vertex.hpp
+++ MoonRuntime/Include/Renderer/Vertex.hpp
@@ -2,6 +2,7 @@
 
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
 #include <glad/gl.h>
 
 template <typename T1>
@@ -52,7 +53,7 @@ public:
         glVertexAttribPointer(1, sizeof(T2) / sizeof(float), GL_FLOAT, GL_FALSE, sizeof(Vertex3D<T1, T2, T3>), (void*)(sizeof(T1)));
         glEnableVertexAttribArray(1);
         glVertexAttribPointer(2, sizeof(T3) / sizeof(float), GL_FLOAT, GL_FALSE, sizeof(Vertex3D<T1, T2, T3>), (void*)(sizeof(T1) + sizeof(T2)));
-        glEnableVertexAttribArray(1);
+        glEnableVertexAttribArray(2);
     }
 
 private:
@@ -74,9 +75,9 @@ public:
         glVertexAttribPointer(1, sizeof(T2) / sizeof(float), GL_FLOAT, GL_FALSE, sizeof(Vertex4D<T1, T2, T3, T4>), (void*)(sizeof(T1)));
         glEnableVertexAttribArray(1);
         glVertexAttribPointer(2, sizeof(T3) / sizeof(float), GL_FLOAT, GL_FALSE, sizeof(Vertex4D<T1, T2, T3, T4>), (void*)(sizeof(T1) + sizeof(T2)));
-        glEnableVertexAttribArray(1);
+        glEnableVertexAttribArray(2);
         glVertexAttribPointer(2, sizeof(T4) / sizeof(float), GL_FLOAT, GL_FALSE, sizeof(Vertex4D<T1, T2, T3, T4>), (void*)(sizeof(T1) + sizeof(T2) + sizeof(T3)));
-        glEnableVertexAttribArray(1);
+        glEnableVertexAttribArray(3);
     }
 
 private:
@@ -85,3 +86,16 @@ private:
     T3 data3;
     T4 data4;
 };
+
+typedef Vertex1D<glm::vec2> Vertex1V2;
+typedef Vertex1D<glm::vec3> Vertex1V3;
+typedef Vertex1D<glm::vec4> Vertex1V4;
+typedef Vertex2D<glm::vec2, glm::vec2> Vertex2V2;
+typedef Vertex2D<glm::vec3, glm::vec3> Vertex2V3;
+typedef Vertex2D<glm::vec4, glm::vec4> Vertex2V4;
+typedef Vertex3D<glm::vec2, glm::vec2, glm::vec2> Vertex3V2;
+typedef Vertex3D<glm::vec3, glm::vec3, glm::vec3> Vertex3V3;
+typedef Vertex3D<glm::vec4, glm::vec4, glm::vec4> Vertex3V4;
+typedef Vertex4D<glm::vec2, glm::vec2, glm::vec2, glm::vec2> Vertex4V2;
+typedef Vertex4D<glm::vec3, glm::vec3, glm::vec3, glm::vec3> Vertex4V3;
+typedef Vertex4D<glm::vec4, glm::vec4, glm::vec4, glm::vec4> Vertex4V4;
