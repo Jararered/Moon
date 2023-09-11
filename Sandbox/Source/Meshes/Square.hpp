37f2b5709d091cd3d4f6436ae1838c2202dc3de5
Jararered <Jararered@icloud.com>
2023-09-11 11:19:28 -0400

    renamed mesh files

--- Sandbox/Source/Meshes/Square.hpp
+++ Sandbox/Source/Meshes/Square.hpp
@@ -22,4 +22,22 @@ public:
 
         VertexBuffer.UploadToGPU();
     }
+
+    SquareMesh(float brightness = 1.0f)
+    {
+        using Vertex = Vertex3D<glm::vec3, glm::vec3, glm::vec2>;
+
+        auto& vertices = VertexBuffer.GetVertices();
+        vertices.reserve(4);
+        vertices.emplace_back(Vertex({+0.5f, -0.5f, 0.0f}, {brightness, brightness, brightness}, {1.0f, 0.0f}));
+        vertices.emplace_back(Vertex({+0.5f, +0.5f, 0.0f}, {brightness, brightness, brightness}, {1.0f, 1.0f}));
+        vertices.emplace_back(Vertex({-0.5f, +0.5f, 0.0f}, {brightness, brightness, brightness}, {0.0f, 1.0f}));
+        vertices.emplace_back(Vertex({-0.5f, -0.5f, 0.0f}, {brightness, brightness, brightness}, {0.0f, 0.0f}));
+
+        auto& indices = VertexBuffer.GetIndices();
+        indices.reserve(6);
+        indices.insert(VertexBuffer.GetIndices().end(), {0, 1, 2, 2, 3, 0});
+
+        VertexBuffer.UploadToGPU();
+    }
 };
