6fcddd5ed649b667eeb19e3212e719f277f85bc2
Jararered <Jararered@icloud.com>
2023-09-12 14:04:32 -0400

    simplifying the window class, will systemize this later

--- MoonRuntime/Source/Window/OpenGLWindow.cpp
+++ MoonRuntime/Source/Window/OpenGLWindow.cpp
@@ -8,51 +8,23 @@
 
 OpenGLWindow::OpenGLWindow(const WindowSpecification& spec) : Window(spec)
 {
-    if (!glfwInit())
-    {
-        std::println("Failed to initialize GLFW");
-    }
-
 #ifdef UNIX
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_TRUE);
 #endif
-    const int major(3), minor(3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, major);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minor);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Create a windowed mode window and its OpenGL context
-    p_GLFWwindow = glfwCreateWindow(spec.Width, spec.Height, "Moon", NULL, NULL);
-    if (!p_GLFWwindow)
-    {
-        std::println("Failed to create OpenGL window");
-        glfwTerminate();
-        return;
-    }
-
-    // Make the window's context current
+    glfwInit();
+    p_GLFWwindow = glfwCreateWindow(spec.Width, spec.Height, spec.Title.c_str(), NULL, NULL);
     glfwMakeContextCurrent(p_GLFWwindow);
 
-    const auto version = gladLoadGL(glfwGetProcAddress);
-    if (version == 0)
-    {
-        std::println("Failed to initialize OpenGL context");
-        glfwTerminate();
-        return;
-    }
-
-    glfwSetWindowUserPointer(p_GLFWwindow, this);
-
-    if (spec.VSync)
-        glfwSwapInterval(1);
-    else
-        glfwSwapInterval(0);
+    gladLoadGL(glfwGetProcAddress);
 }
 
 OpenGLWindow::~OpenGLWindow()
 {
-    glfwWindowShouldClose(p_GLFWwindow);
 }
 
 void OpenGLWindow::Update(float dt)
--- MoonRuntime/Source/Window/Window.hpp
+++ MoonRuntime/Source/Window/Window.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Components/WindowSpecification.hpp"
+#include "WindowSpecification.hpp"
 
 #include <vector>
 
