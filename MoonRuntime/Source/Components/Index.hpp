f709a5112b0526a335de07e25da38441e1aff10e
Jararered <Jararered@icloud.com>
2023-09-12 14:03:34 -0400

    index simplification

--- MoonRuntime/Source/Components/Index.hpp
+++ MoonRuntime/Source/Components/Index.hpp
@@ -1,16 +1,13 @@
 #pragma once
 
-#include <glm/vec3.hpp>
+#include <glm/glm.hpp>
 
 typedef glm::ivec3 Index;
 
-static Index GetIndex(const glm::vec3& position, unsigned int scale)
+static Index GetIndex(const glm::vec3& position, float scale)
 {
     assert(scale > 0);
 
-    Index index;
-    position.x / scale < 0 ? index.x = position.x / scale - 1 : index.x = position.x / scale;
-    position.y / scale < 0 ? index.y = position.y / scale - 1 : index.y = position.y / scale;
-    position.z / scale < 0 ? index.z = position.z / scale - 1 : index.z = position.z / scale;
+    glm::ivec3 index = glm::floor(position / scale);
     return index;
 }
