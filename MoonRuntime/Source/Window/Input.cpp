b018ba0fd68e6b777f5fb905759da8639d8d1def
Jararered <Jararered@icloud.com>
2023-09-27 10:27:20 -0400

    some simplifications

--- MoonRuntime/Source/Window/Input.cpp
+++ MoonRuntime/Source/Window/Input.cpp
@@ -16,14 +16,12 @@ bool Input::IsMouseButtonPressed(int mouseButtonCode)
 
 bool Input::IsLeftClick()
 {
-    const auto state = glfwGetMouseButton(glfwGetCurrentContext(), Mouse::ButtonLeft);
-    return state == GLFW_PRESS;
+    return IsMouseButtonPressed(Mouse::ButtonLeft);
 }
 
 bool Input::IsRightClick()
 {
-    const auto state = glfwGetMouseButton(glfwGetCurrentContext(), Mouse::ButtonRight);
-    return state == GLFW_PRESS;
+    return IsMouseButtonPressed(Mouse::ButtonRight);
 }
 
 glm::vec2 Input::GetMousePosition()
@@ -52,22 +50,24 @@ glm::vec2 Input::GetCapturedMouseMovement()
 
 void Input::CaptureCursor()
 {
+    const auto context = glfwGetCurrentContext();
     if (glfwRawMouseMotionSupported())
-        glfwSetInputMode(glfwGetCurrentContext(), GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+        glfwSetInputMode(context, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
 
-    glfwSetInputMode(glfwGetCurrentContext(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetInputMode(context, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // Need to center cursor before cursor position callback is run
     // Prevents a possibly large xPosition/yPosition when entering the window
-    glfwSetCursorPos(glfwGetCurrentContext(), 0.0, 0.0);
+    glfwSetCursorPos(context, 0.0, 0.0);
 }
 
 void Input::ReleaseCursor()
 {
+    const auto context = glfwGetCurrentContext();
     auto position = glm::ivec2(0);
-    glfwGetWindowSize(glfwGetCurrentContext(), &position.x, &position.y);
-    glfwSetInputMode(glfwGetCurrentContext(), GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-    glfwSetCursorPos(glfwGetCurrentContext(), static_cast<double>(position.x / 2), static_cast<double>(position.y / 2));
+    glfwGetWindowSize(context, &position.x, &position.y);
+    glfwSetInputMode(context, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    glfwSetCursorPos(context, static_cast<double>(position.x / 2), static_cast<double>(position.y / 2));
 }
 
 void Input::PollEvents()
