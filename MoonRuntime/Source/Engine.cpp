e1e3a5bdabe88f40cfd1837946e56ffd564912e0
Jararered <Jararered@icloud.com>
2023-10-10 07:18:12 -0400

    removed random textures, more debug texs

--- MoonRuntime/Source/Engine.cpp
+++ MoonRuntime/Source/Engine.cpp
@@ -23,195 +23,198 @@
 #include <glfw/glfw3.h>
 #include <imgui.h>
 
-void Engine::Initialize()
+namespace Moon
 {
-    if (m_Status = EngineStatus::Initialized)
-        return;
-
-    m_Scenario = std::make_shared<Scenario>();
-
-    m_Scenario->Initialize();
-    m_Scenario->RegisterComponent<Camera>();
-    m_Scenario->RegisterComponent<Control>();
-    m_Scenario->RegisterComponent<Mesh>();
-    m_Scenario->RegisterComponent<RigidBody>();
-    m_Scenario->RegisterComponent<Script>();
-    m_Scenario->RegisterComponent<Shader>();
-    m_Scenario->RegisterComponent<Texture>();
-    m_Scenario->RegisterComponent<Transform>();
-    m_Scenario->RegisterComponent<TestComponent>();
-
-    // Emplace back systems in initialization and update order
-    m_SystemMap.emplace(1, m_Scenario->RegisterSystem<ControlSystem>());
-    m_SystemMap.emplace(2, m_Scenario->RegisterSystem<CameraSystem>());
-    m_SystemMap.emplace(3, m_Scenario->RegisterSystem<ScriptSystem>());
-    m_SystemMap.emplace(4, m_Scenario->RegisterSystem<PhysicsSystem>());
-    m_SystemMap.emplace(5, m_Scenario->RegisterSystem<RenderSystem>());
-
-    for (const auto [_, system] : m_SystemMap)
+    void Engine::Initialize()
     {
-        system->Register(m_Scenario);
+        if (m_Status = EngineStatus::Initialized)
+            return;
+
+        m_Scenario = std::make_shared<Scenario>();
+
+        m_Scenario->Initialize();
+        m_Scenario->RegisterComponent<Camera>();
+        m_Scenario->RegisterComponent<Control>();
+        m_Scenario->RegisterComponent<Mesh>();
+        m_Scenario->RegisterComponent<RigidBody>();
+        m_Scenario->RegisterComponent<Script>();
+        m_Scenario->RegisterComponent<Shader>();
+        m_Scenario->RegisterComponent<Texture>();
+        m_Scenario->RegisterComponent<Transform>();
+        m_Scenario->RegisterComponent<TestComponent>();
+
+        // Emplace back systems in initialization and update order
+        m_SystemMap.emplace(1, m_Scenario->RegisterSystem<ControlSystem>());
+        m_SystemMap.emplace(2, m_Scenario->RegisterSystem<CameraSystem>());
+        m_SystemMap.emplace(3, m_Scenario->RegisterSystem<ScriptSystem>());
+        m_SystemMap.emplace(4, m_Scenario->RegisterSystem<PhysicsSystem>());
+        m_SystemMap.emplace(5, m_Scenario->RegisterSystem<RenderSystem>());
+
+        for (const auto [_, system] : m_SystemMap)
+        {
+            system->Register(m_Scenario);
+        }
+
+        for (const auto [_, system] : m_SystemMap)
+        {
+            system->Initialize();
+        }
+
+        m_Status = EngineStatus::Initialized;
     }
 
-    for (const auto [_, system] : m_SystemMap)
+    void Engine::Start()
     {
-        system->Initialize();
+        if (!p_Window or m_Status == EngineStatus::Uninitialized)
+            return;
+
+        while (p_Window->IsRunning())
+        {
+            const auto frameStartTime = std::chrono::high_resolution_clock::now();
+
+            p_Window->NewFrame();
+
+            for (const auto [systemID, system] : m_SystemMap)
+            {
+                system->Update(m_DT);
+            }
+
+            UpdateUI();
+
+            p_Window->EndFrame();
+
+            const auto frameEndTime = std::chrono::high_resolution_clock::now();
+            m_DT = std::chrono::duration<float, std::chrono::seconds::period>(frameEndTime - frameStartTime).count();
+        }
     }
 
-    m_Status = EngineStatus::Initialized;
-}
-
-void Engine::Start()
-{
-    if (!p_Window or m_Status == EngineStatus::Uninitialized)
-        return;
-
-    while (p_Window->IsRunning())
+    std::shared_ptr<Window> Engine::CreateWindow(const WindowSpecification& spec)
     {
-        const auto frameStartTime = std::chrono::high_resolution_clock::now();
+        p_Window = std::make_shared<OpenGLWindow>(spec);
 
-        p_Window->NewFrame();
+        if (m_Status == EngineStatus::Uninitialized)
+            Initialize();
 
+        return p_Window;
+    }
+
+    void Engine::UpdateUI()
+    {
+        ImGui::Begin("Engine Systems", NULL, ImGuiWindowFlags_AlwaysAutoResize);
         for (const auto [systemID, system] : m_SystemMap)
         {
-            system->Update(m_DT);
-        }
-
-        UpdateUI();
-
-        p_Window->EndFrame();
-
-        const auto frameEndTime = std::chrono::high_resolution_clock::now();
-        m_DT = std::chrono::duration<float, std::chrono::seconds::period>(frameEndTime - frameStartTime).count();
-    }
-}
-
-std::shared_ptr<Window> Engine::CreateWindow(const WindowSpecification& spec)
-{
-    p_Window = std::make_shared<OpenGLWindow>(spec);
-
-    if (m_Status == EngineStatus::Uninitialized)
-        Initialize();
-
-    return p_Window;
-}
-
-void Engine::UpdateUI()
-{
-    ImGui::Begin("Engine Systems", NULL, ImGuiWindowFlags_AlwaysAutoResize);
-    for (const auto [systemID, system] : m_SystemMap)
-    {
-        if (ImGui::CollapsingHeader(system->m_Name.c_str()))
-        {
-            system->UpdateUI();
-        }
-    }
-
-    ImGui::End();
-
-    static Entity selectedEntity;
-    static Transform selectedTransform;
-    static RigidBody selectedRigidBody;
-    static std::string selectedMesh = "None";
-    static std::string selectedTexture = "None";
-    static std::string selectedScript = "None";
-    static std::string selectedShader = "None";
-
-    ImGui::Begin("Entity Creator", NULL, ImGuiWindowFlags_AlwaysAutoResize);
-
-    auto fmt = std::string("Current entity: ") + std::to_string(selectedEntity);
-    ImGui::LabelText("", fmt.c_str());
-
-    ImGui::LabelText("", "Transform");
-    ImGui::InputFloat3("Position", &selectedTransform.Position.x);
-    ImGui::InputFloat3("Rotation", &selectedTransform.Rotation.x);
-    ImGui::InputFloat3("Scale", &selectedTransform.Scale.x);
-
-    ImGui::LabelText("", "Rigid Body");
-    ImGui::InputFloat3("Velocity", &selectedRigidBody.Velocity.x);
-    ImGui::InputFloat("Mass", &selectedRigidBody.Mass);
-
-    ImGui::LabelText("", "Renderables");
-    if (!m_AvaliableMeshesMap.empty())
-    {
-        if (ImGui::BeginCombo("Avaliable Meshes", selectedMesh.c_str()))
-        {
-            for (const auto [key, mesh] : m_AvaliableMeshesMap)
+            if (ImGui::CollapsingHeader(system->m_Name.c_str()))
             {
-                bool is_selected = (selectedMesh == key.c_str());
-                if (ImGui::Selectable(key.c_str(), is_selected))
-                    selectedMesh = key.c_str();
-
-                if (is_selected)
-                    ImGui::SetItemDefaultFocus();
+                system->UpdateUI();
             }
-            ImGui::EndCombo();
         }
-    }
-    if (!m_AvaliableTexturesMap.empty())
-    {
-        if (ImGui::BeginCombo("Avaliable Textures", selectedTexture.c_str()))
+
+        ImGui::End();
+
+        static Entity selectedEntity;
+        static Transform selectedTransform;
+        static RigidBody selectedRigidBody;
+        static std::string selectedMesh = "None";
+        static std::string selectedTexture = "None";
+        static std::string selectedScript = "None";
+        static std::string selectedShader = "None";
+
+        ImGui::Begin("Entity Creator", NULL, ImGuiWindowFlags_AlwaysAutoResize);
+
+        auto fmt = std::string("Current entity: ") + std::to_string(selectedEntity);
+        ImGui::LabelText("", fmt.c_str());
+
+        ImGui::LabelText("", "Transform");
+        ImGui::InputFloat3("Position", &selectedTransform.Position.x);
+        ImGui::InputFloat3("Rotation", &selectedTransform.Rotation.x);
+        ImGui::InputFloat3("Scale", &selectedTransform.Scale.x);
+
+        ImGui::LabelText("", "Rigid Body");
+        ImGui::InputFloat3("Velocity", &selectedRigidBody.Velocity.x);
+        ImGui::InputFloat("Mass", &selectedRigidBody.Mass);
+
+        ImGui::LabelText("", "Renderables");
+        if (!m_AvaliableMeshesMap.empty())
         {
-            for (const auto [key, mesh] : m_AvaliableTexturesMap)
+            if (ImGui::BeginCombo("Avaliable Meshes", selectedMesh.c_str()))
             {
-                bool is_selected = (selectedTexture == key.c_str());
-                if (ImGui::Selectable(key.c_str(), is_selected))
-                    selectedTexture = key.c_str();
-                if (is_selected)
-                    ImGui::SetItemDefaultFocus();
+                for (const auto [key, mesh] : m_AvaliableMeshesMap)
+                {
+                    bool is_selected = (selectedMesh == key.c_str());
+                    if (ImGui::Selectable(key.c_str(), is_selected))
+                        selectedMesh = key.c_str();
+
+                    if (is_selected)
+                        ImGui::SetItemDefaultFocus();
+                }
+                ImGui::EndCombo();
             }
-            ImGui::EndCombo();
         }
-    }
-    if (!m_AvaliableScriptsMap.empty())
-    {
-        if (ImGui::BeginCombo("Avaliable Scripts", selectedScript.c_str()))
+        if (!m_AvaliableTexturesMap.empty())
         {
-            bool is_selected = selectedScript == "None";
-            if (ImGui::Selectable("None", is_selected))
-                selectedScript = "None";
-            for (const auto [key, mesh] : m_AvaliableScriptsMap)
+            if (ImGui::BeginCombo("Avaliable Textures", selectedTexture.c_str()))
             {
-                is_selected = (selectedScript == key.c_str());
-                if (ImGui::Selectable(key.c_str(), is_selected))
-                    selectedScript = key.c_str();
-                if (is_selected)
-                    ImGui::SetItemDefaultFocus();
+                for (const auto [key, mesh] : m_AvaliableTexturesMap)
+                {
+                    bool is_selected = (selectedTexture == key.c_str());
+                    if (ImGui::Selectable(key.c_str(), is_selected))
+                        selectedTexture = key.c_str();
+                    if (is_selected)
+                        ImGui::SetItemDefaultFocus();
+                }
+                ImGui::EndCombo();
             }
-            ImGui::EndCombo();
         }
-    }
-    if (!m_AvaliableShadersMap.empty())
-    {
-        if (ImGui::BeginCombo("Avaliable Shaders", selectedShader.c_str()))
+        if (!m_AvaliableScriptsMap.empty())
         {
-            for (const auto [key, mesh] : m_AvaliableShadersMap)
+            if (ImGui::BeginCombo("Avaliable Scripts", selectedScript.c_str()))
             {
-                bool is_selected = (selectedShader == key.c_str());
-                if (ImGui::Selectable(key.c_str(), is_selected))
-                    selectedShader = key.c_str();
-                if (is_selected)
-                    ImGui::SetItemDefaultFocus();
+                bool is_selected = selectedScript == "None";
+                if (ImGui::Selectable("None", is_selected))
+                    selectedScript = "None";
+                for (const auto [key, mesh] : m_AvaliableScriptsMap)
+                {
+                    is_selected = (selectedScript == key.c_str());
+                    if (ImGui::Selectable(key.c_str(), is_selected))
+                        selectedScript = key.c_str();
+                    if (is_selected)
+                        ImGui::SetItemDefaultFocus();
+                }
+                ImGui::EndCombo();
             }
-            ImGui::EndCombo();
         }
+        if (!m_AvaliableShadersMap.empty())
+        {
+            if (ImGui::BeginCombo("Avaliable Shaders", selectedShader.c_str()))
+            {
+                for (const auto [key, mesh] : m_AvaliableShadersMap)
+                {
+                    bool is_selected = (selectedShader == key.c_str());
+                    if (ImGui::Selectable(key.c_str(), is_selected))
+                        selectedShader = key.c_str();
+                    if (is_selected)
+                        ImGui::SetItemDefaultFocus();
+                }
+                ImGui::EndCombo();
+            }
+        }
+
+        if (ImGui::Button("Create Entity"))
+        {
+            selectedEntity = m_Scenario->CreateEntity();
+            m_Scenario->AddComponent<Transform>(selectedEntity, selectedTransform);
+            m_Scenario->AddComponent<RigidBody>(selectedEntity, selectedRigidBody);
+
+            if (selectedMesh != "None")
+                m_Scenario->AddComponent<Mesh>(selectedEntity, m_AvaliableMeshesMap[selectedMesh]);
+            if (selectedTexture != "None")
+                m_Scenario->AddComponent<Texture>(selectedEntity, m_AvaliableTexturesMap[selectedTexture]);
+            if (selectedScript != "None")
+                m_Scenario->AddComponent<Script>(selectedEntity, m_AvaliableScriptsMap[selectedScript]);
+            if (selectedShader != "None")
+                m_Scenario->AddComponent<Shader>(selectedEntity, m_AvaliableShadersMap[selectedShader]);
+        }
+
+        ImGui::End();
     }
-
-    if (ImGui::Button("Create Entity"))
-    {
-        selectedEntity = m_Scenario->CreateEntity();
-        m_Scenario->AddComponent<Transform>(selectedEntity, selectedTransform);
-        m_Scenario->AddComponent<RigidBody>(selectedEntity, selectedRigidBody);
-
-        if (selectedMesh != "None")
-            m_Scenario->AddComponent<Mesh>(selectedEntity, m_AvaliableMeshesMap[selectedMesh]);
-        if (selectedTexture != "None")
-            m_Scenario->AddComponent<Texture>(selectedEntity, m_AvaliableTexturesMap[selectedTexture]);
-        if (selectedScript != "None")
-            m_Scenario->AddComponent<Script>(selectedEntity, m_AvaliableScriptsMap[selectedScript]);
-        if (selectedShader != "None")
-            m_Scenario->AddComponent<Shader>(selectedEntity, m_AvaliableShadersMap[selectedShader]);
-    }
-
-    ImGui::End();
 }
